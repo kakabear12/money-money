f0f175470a92a4ada910ff20c8abf0e1d8d1ff30
trungnt <trung2kgl@gmail.com>
2023-06-04 01:40:15 +0700

    [Update]  fix something

--- src/main/java/com/example/moneymoney/config/CorsConfig.java
+++ src/main/java/com/example/moneymoney/config/CorsConfig.java
@@ -7,14 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-				.allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
-						HttpMethod.OPTIONS.name(), HttpMethod.DELETE.name())
-				.allowedHeaders("*")
-				.allowedOrigins("*");
-	}
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		registry.addMapping("/**")
+//				.allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
+//						HttpMethod.OPTIONS.name(), HttpMethod.DELETE.name())
+//				.allowedHeaders("*")
+//				.allowedOrigins("*");
+//	}
 
 
 }
--- src/main/java/com/example/moneymoney/config/WebSecurityConfig.java
+++ src/main/java/com/example/moneymoney/config/WebSecurityConfig.java
@@ -22,6 +22,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 @Configuration
@@ -75,6 +80,19 @@ public class WebSecurityConfig {
 
     @Autowired
     private JwtTokenFilter jwtTokenFilter;
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*")); // Cho phép truy cập từ tất cả các nguồn
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); // Cho phép các phương thức HTTP
+        configuration.setAllowedHeaders(Arrays.asList("*")); // Cho phép tất cả các header
+        configuration.setAllowCredentials(true); // Cho phép chia sẻ cookie khi sử dụng CORS
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
     @Bean
     public HttpCookieOAuth2AuthorizationRequestRepository cookieAuthorizationRequestRepository() {
         return new HttpCookieOAuth2AuthorizationRequestRepository();
